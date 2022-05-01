681312c9703da203bc479467f3c0d501a93c62e9
arshiha bristy <arshihabristy5@gmail.com>
2022-05-02 01:34:34 +0600

    other part

--- src/components/Home/Dry/Dry.js
+++ src/components/Home/Dry/Dry.js
@@ -2,17 +2,23 @@ import React from "react";
 import "./Dry.css";
 
 const Dry = ({ dry }) => {
-  const { name, img } = dry;
+  const { name, img, price, suplierName, quantity } = dry;
   return (
     <div className="g-5 col-sm-12 col-md-6 col-lg-4">
       <div class="card" style={{ width: "18rem" }}>
-        <img src={img} class="card-img-top" alt="..." />
+        <div className="img-style">
+          <img src={img} class="card-img-top" alt="..." />
+        </div>
         <div class="card-body">
           <h5 class="card-title">{name}</h5>
+
           <p class="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
+          <h6>SuplierName: Md.Piyas Shekh{suplierName}</h6>
+          <h6>Quantity: 1Kg{quantity}</h6>
+          <p>Price: 300{price}</p>
           <a href="#" class="btn btn-success">
             Book
           </a>
