01acf25418208d63d7b9e8ecf2bdb0991d4998a6
arshiha bristy <arshihabristy5@gmail.com>
2022-05-02 15:00:22 +0600

    footer added

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,11 +1,19 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>
-        <small>copyright @ 2022</small>
-      </p>
+    <footer className="footer">
+      <div className="row text-white">
+        <div className="col-lg-6 text-center text-lg-left">
+          <p className="small mb-0 mt-1">
+            Copyright &copy;2022 <br></br>designed by Arshiha Bristy
+          </p>
+        </div>
+        <div className="col-lg-6 text-center text-lg-right">
+          <a href="#">Home</a>
+        </div>
+      </div>
     </footer>
   );
 };
