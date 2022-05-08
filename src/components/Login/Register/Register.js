0d81db7f3018b1a11be6b5b4a9aa25a51ac68541
arshiha bristy <arshihabristy5@gmail.com>
2022-05-08 16:19:31 +0600

    register done

--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -1,23 +1,29 @@
 import React from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init.js";
 import "./Register.css";
 
 const Register = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
   const navigate = useNavigate();
 
   const navigateLogin = () => {
     navigate("/login");
   };
 
+  if (user) {
+    navigate("/home");
+  }
+
   const handleRegister = (event) => {
     event.preventDefault();
     const name = event.target.name.value;
     const email = event.target.email.value;
-      const password = event.target.password.value;
-      
+    const password = event.target.password.value;
 
-
-      
+    createUserWithEmailAndPassword(email, password);
   };
   return (
     <div className="register">
