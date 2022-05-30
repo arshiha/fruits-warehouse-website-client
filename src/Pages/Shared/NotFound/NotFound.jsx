4078e8d243dc4a02a6e858849bea6afccdb4aa5e
arshiha bristy <arshihabristy5@gmail.com>
2022-05-30 14:31:48 +0600

    update

--- src/Pages/Shared/NotFound/NotFound.jsx
+++ src/Pages/Shared/NotFound/NotFound.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import PageNotFound from '../PageNotFound/PageNotFound.jsx';
-import'./NotFound.css'
+import React from "react";
+import PageNotFound from "../PageNotFound/PageNotFound.jsx";
+import "./NotFound.css";
 const NotFound = () => {
-    return (
-        <div className='page-error mt-5'>
-            <h3 className='text-center text'>Oops! Something went wrong.</h3>
-             <PageNotFound></PageNotFound>
-        </div>
-    );
+  return (
+    <div className="page-error mt-2">
+      <h3 className="text-center text">
+        Oops! Something went wrong.Please try again...
+      </h3>
+      <PageNotFound></PageNotFound>
+    </div>
+  );
 };
 
 export default NotFound;
