bb4f1b6eadeca1dbcf04397ab1761e118b5f6391
arshiha bristy <arshihabristy5@gmail.com>
2022-06-07 21:55:57 +0600

    added assanment 10

--- src/Pages/Shared/NotFound/NotFound.jsx
+++ src/Pages/Shared/NotFound/NotFound.jsx
@@ -5,11 +5,13 @@ const NotFound = () => {
   return (
     <div className="page-error mt-5">
       <h3 className="text-center text">
-        Oops! Something went wrong.Please try again...please try again.....
+        Oops! Something went wrong.Please try again...
       </h3>
       <PageNotFound></PageNotFound>
     </div>
   );
 };
+
+// assainment 10 private to public
 
 export default NotFound;
